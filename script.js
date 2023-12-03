d9ce5704f32c58e1eb735119f4d5abb5b2745c4b
TaylorReis-lab <reist98822@gmail.com>
2023-12-03 17:57:39 -0300

    Update js

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const buttonDraw = document.querySelector(".button-start")
 function drawRole() {
     const min = Math.ceil (document.querySelector(".input-one").value)
     const max = Math.floor (document.querySelector(".input-two").value)
-    const valueResult = Math.floor(Math.random() * (max - min + 1)) + min;
-    alert("O número sorteado foi " + valueResult)
+    
+    if(min >= max){
+        alert("O valor minimo tem que ser MENOR que o valor maximo")
+    } else {
+        const valueResult = Math.floor(Math.random() * (max - min + 1)) + min;
+        alert("O número sorteado foi " + valueResult)
+    }
 }
